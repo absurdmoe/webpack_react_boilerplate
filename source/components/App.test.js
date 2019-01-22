fa4f068ce529d65abfb9b7056d77c317c7a5668d
rathod-jay <chris.tang@cognizant.com>
2019-01-21 20:58:13 -0800

    removed unnecessary methods

--- source/components/App.test.js
+++ source/components/App.test.js
@@ -3,20 +3,10 @@ import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdaptor from 'enzyme-adapter-react-16';
 import App from './App';
 
-const setup = (props = {}, state = null) => {
-  const wrapper = shallow(<App {...props} />);
-  if (state) wrapper.setState(state);
-  return wrapper;
-};
-
-const findByAttribute = (wrapper, val) => {
-  return wrapper.find('#App-Component');
-};
-
 Enzyme.configure({ adapter: new EnzymeAdaptor() });
 
 test('App renders without error', () => {
-  const wrapper = setup();
-  const appComponent = findByAttribute(wrapper, 'App');
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find('#App-Component');
   expect(appComponent.length).toBe(1);
 });
